96b8f2b85216d2decfb0f896cd072477d3a50d3c
wiktor <wwolakksiazek@fream.pl>
2023-07-26 20:47:57 +0200

    Moved ScoreHelper and added tests

---
+++ src/main/java/com/vonage/api/interview/search/ScoreHelper.java
@@ -0,0 +1,30 @@
+package com.vonage.api.interview.search;
+
+public class ScoreHelper {
+
+    private final int allWordsCount;
+    private int wordOccurrenceCount = 0;
+
+    public ScoreHelper(int allWordsCount) {
+        if(allWordsCount < 0){
+            throw new IllegalArgumentException("allWordsCount must be greater than zero");
+        }
+        this.allWordsCount = allWordsCount;
+    }
+
+    public void incrementWordOccurrenceCount() {
+        int incrementedWordOccurrenceCount = wordOccurrenceCount + 1;
+        if(incrementedWordOccurrenceCount > allWordsCount){
+            throw new IllegalStateException("Cannot call increment method. wordOccurrenceCount must be less than or equal to allWordsCount");
+        }
+
+        wordOccurrenceCount = incrementedWordOccurrenceCount;
+    }
+
+    public int getScore() {
+        if(allWordsCount == 0){
+            return 0;
+        }
+        return (int) (((double) wordOccurrenceCount / allWordsCount) * 100);
+    }
+}
--- src/test/java/com/vonage/api/interview/test/ScoreHelperTest.java
+++
@@ -1,7 +0,0 @@
-package com.vonage.api.interview.test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class ScoreHelperTest {
-
-}
